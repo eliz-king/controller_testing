93b3029aae117b476d2e4e8f1bca2a46754c6c8a
Elizabeth King <elizking.dev@gmail.com>
2019-03-12 21:02:11 -0600

    broken testing stuff

--- spec/controllers/bank_accounts_controller_spec.rb
+++ spec/controllers/bank_accounts_controller_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe BankAccountsController, type: :controller do
+  # this will log in
   login_user
-  #method from controller macros to create user & log in
 
-#creating 2 variables of hashes:
   let(:valid_attr) {
-    { amount: 200,
-      institution: 'Chase',
+    {
+      amount: 200,
+      institution: "Chase",
       active: true
     }
-  } #this should pass
+  }
 
   let(:invalid_attr) {
-    { amount: 200,
+    {
+      amount: 200,
       institution: '',
       active: true
     }
-  } #this should fail
+  }
 
-#anytime expecting just a webpage this how test looks:
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -27,17 +27,15 @@ RSpec.describe BankAccountsController, type: :controller do
     end
   end
 
-#should show a particular bank acct
   describe "GET #show" do
     it "returns http success" do
-      bank_account = @user.bank_account.create! valid_attr
-      #BankAccount.find(params[:id]) - have to test for this
+      bank_account = @user.bank_accounts.create! valid_attr
+      # BankAccount.find(params[:id])
       get :show, params: { id: bank_account.id }
       expect(response).to have_http_status(:success)
     end
   end
 
-#shows form
   describe "GET #new" do
     it "returns http success" do
       get :new
@@ -45,76 +43,117 @@ RSpec.describe BankAccountsController, type: :controller do
     end
   end
 
-#shows form
   describe "GET #edit" do
     it "returns http success" do
-      bank_account = @user.bank_accounts.create! valid_attr #using var created abve
-      get :edit, prams: { id: bank_account.id }
+      bank_account = @user.bank_accounts.create! valid_attr
+      get :edit, params: { id: bank_account.id }
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "Post/create" do
+  describe "Post / create" do
     context "with valid Params" do
       # def create
       #   @bank_account = BankAccount.new(bank_account_params)
       #   if @bank_account.save
-      #     redirect_to @bank_account (or could be bank_accounts_path)
+      #     redirect_to @bank_account
       #   else
       #     render :new
       #   end
-      # end - this creates a record in db & redirects
+      # end
+
       it "creates a new bank account" do
         expect {
-          post :create, params: {bank_account: valid_attr}
-        }.to change(BankAccount, :count).by(1) #should add a bank acct
+          post :create, params: { bank_account: valid_attr }
+        }.to change(BankAccount, :count).by(1)
       end
 
-      it "redirect to the new bank acct" do
-        post :create, params: {bank_account: valid_attr}
-        expect(response).to redirect_to(BankAccount.last) #new record will be the last one in the record
+      it "redirect to the new bank account" do
+        post :create, params: { bank_account: valid_attr }
+        expect(response).to redirect_to(BankAccount.last)
       end
     end
 
     context "with invalid params" do
-      it "does not create a new bank acct" do
-        expect {
-          post :create, params: {bank_account: invalid_attr}
+      it "does not create a new bank account" do
+        expect { 
+          post :create, params: { bank_account: invalid_attr }
         }.to change(BankAccount, :count).by(0)
       end
 
       it "renders the new form" do
-      post :create, params: {bank_account: invalid_attr}
-      expect(response).to be_successful
+        post :create, params: { bank_account: invalid_attr }
+        expect(response).to be_successful
       end
     end
   end
 
-  describe "put / pudate" do
-    # find the bank account we're updating
+  describe "Put / update" do
+    # find the bankaccount we are updating
     # BankAccount.find(params[:id])
-    # if @bank_accountupdate(bank_account_params)
+    # if @bank_account.update(bank_account_params)
       # redirect_to @bank_account
-    #else
-      # doesn't update
+    # else
+      # doesn't update 
       # render :edit form
-    context "with valid params" do
-      it "update a bank account that is requested" do
+    let(:new_attr) {
+      { amount: 500 }
+    }
 
+    before(:each) do
+      @bank_account =  @user.bank_accounts.create! valid_attr
+    end
+
+    context "with vaild params" do
+
+      before(:each) do
+        put :update, params: { id: @bank_account.id, bank_account: new_attr }
+      end
+
+      it "update a bank account that is requested" do
+        @bank_account.reload
+        expect(@bank_account.amount).to eq(new_attr[:amount])
       end
 
       it "redirect to the bank account" do
-
+        expect(response).to redirect_to(@bank_account)
       end
     end
 
-    context "with invalid params" do
+    context "with invaild params" do 
+      before(:each) do
+        put :update, params: { id: @bank_account.id, bank_account: invalid_attr }
+      end
+
       it "does not update the bank account" do
-
+        @bank_account.reload
+        expect(@bank_account.institution).to_not eq(invalid_attr[:institution])
       end
-      it 'renders the edit form' do
 
+      it "renders the edit form" do
+        expect(response).to be_successful
       end
+    end
+  end
+
+  describe "delete / destroy" do
+    # find a requested bank account 
+    # @bank_account.destroy
+    # redirect_to bank_accounts_url, notice: "Bank account has been destroyed" 
+
+    before(:each) do
+      @bank_account = @user.bank_accounts.create! valid_attr
+    end
+
+    it "delete a requested bank acount" do
+      expect {
+        delete :destroy, params: { id: @bank_account.id }
+      }.to change(BankAccount, :count).by(-1)
+    end
+
+    it "redirect to index" do
+      delete :destroy, params: { id: @bank_account.id }
+      expect(response).to redirect_to(bank_accounts_url)
     end
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -6,6 +7,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'simplecov'
+require 'support/controller_macros'
 SimpleCov.start
 
 Shoulda::Matchers.configure do |config|
